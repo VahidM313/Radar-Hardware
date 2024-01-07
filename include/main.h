ae7c0d12e375077d6813b290a0d6c577e28508ff
Vahid Mohammdi <vahidood@gmail.com>
2024-01-07 22:26:09 +0330

    basic code for servo and connect esp32 to local mosquitto

--- include/main.h
+++ include/main.h
@@ -4,24 +4,20 @@
 #include <Arduino.h>
 #include <ArduinoJson.h>
 #include "driver/timer.h"
-#include <Servo.h>
 #include <SevSeg.h>
 #include <NewPing.h>
 #include <WiFi.h>
 #include <PubSubClient.h>
+#include <ESP32Servo.h>
 
 #define IrPin 35
-#define servoPin 16
+#define servoPin 16 //rx2 pin
 #define triggerPin 23
 #define echoPin 22
 #define maxDistance 450
 
-#define TIMER_DIVIDER 16
-#define TIMER_SCALE static_cast<int>(TIMER_BASE_CLK / TIMER_DIVIDER)
-#define TIMER_INTERVAL_SEC (0.001)
-#define TEST_WITH_RELOAD 1
-
 void setupWifi();
 void reconnect();
 void publishData(int distance,int degree);
+void calculateDistance();
 #endif
